82cd1919a0088724dfdcb541c1f975b299f3b5cc
justyna2104 <justynaharmuszkiewicz@gmail.com>
2021-05-31 12:31:39 +0200

    Trying to add searching by category

---
+++ Models/RecipeCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektDotnet.Models
+{
+    public class RecipeCategory
+    {
+        public int RecipeId { get; set; }
+
+        [ForeignKey("RecipeId")]
+        public Recipe Recipe { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+}
