c48205e2ef437d06bf82557f291b817c93edfa49
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-06-10 19:14:30 +0200

    Update Profile Page

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ProjektDotnet.Data;
+using ProjektDotnet.Models;
 
 namespace ProjektDotnet.Pages
 {
     public class ProfileModel : PageModel
     {
         private readonly ILogger<ProfileModel> _logger;
+        private readonly ApplicationDbContext _context;
+
         public string UserName { get; set; }
-        public ProfileModel(ILogger<ProfileModel> logger)
+        public List<Recipe> Recipes { get; set; }
+        public ProfileModel(ILogger<ProfileModel> logger,ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet(string username)
         {
             UserName = username;
+            Recipes = _context.Recipe.Where(r=>r.User.UserName == username).OrderByDescending(d=>d.Date).ToList();
         }
     }
 }
