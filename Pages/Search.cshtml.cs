33b8f30c29603e2ef6a6af124f442e57deeb52ba
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-06-10 19:14:59 +0200

    Formating Search Page

--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -25,31 +25,31 @@ namespace ProjektDotnet.Pages
 
         public SelectList Category { get; set; }
         public string SearchCategory { get; set; }
-        public List<Recipe> RecipeCategories { get; set; } 
-        
+        public List<Recipe> RecipeCategories { get; set; }
+
 
         public async Task OnGetAsync(string searchString, string searchCategory, string searchAuthor)
         {
-            searchString = searchString ?? string.Empty;
-            
-            IQueryable<string> categoryQuery = from c in _context.Category orderby c.Name select c.Name;
+            searchString ??= string.Empty;
 
-            searchAuthor = searchAuthor ?? string.Empty;
-            searchCategory = searchCategory ?? string.Empty;
-            var recipeCategory = (from r in _context.Recipe
-                                                  join rc in _context.RecipeCategory on r.Id equals rc.RecipeId
-                                                  join c in _context.Category on rc.CategoryId equals c.Id
-                                                  join u in _context.Users on r.UserId equals u.Id
-                                                  where c.Name.ToUpper().Contains(searchCategory.ToUpper()) 
-                                                  && r.Name.ToUpper().Contains(searchString.ToUpper())
-                                                  && u.UserName.ToUpper().Contains(searchAuthor.ToUpper())
-                                                  orderby r.Date
-                                  select r
-              );   
+            var categoryQuery = from c in _context.Category orderby c.Name select c.Name;
+
+            searchAuthor ??= string.Empty;
+            searchCategory ??= string.Empty;
+            var recipeCategory = from r in _context.Recipe
+                                 join rc in _context.RecipeCategory on r.Id equals rc.RecipeId
+                                 join c in _context.Category on rc.CategoryId equals c.Id
+                                 join u in _context.Users on r.UserId equals u.Id
+                                 where c.Name.ToUpper().Contains(searchCategory.ToUpper())
+                                 && r.Name.ToUpper().Contains(searchString.ToUpper())
+                                 && u.UserName.ToUpper().Contains(searchAuthor.ToUpper())
+                                 orderby r.Date
+                                 select r
+              ;
 
             Category = new SelectList(await categoryQuery.ToListAsync());
             RecipeCategories = await recipeCategory.ToListAsync();
-            
+
         }
     }
 }
