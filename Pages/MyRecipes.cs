e25ea52657e037b7dba26ca8ec56a7760a79c9b3
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-06-13 14:05:50 +0200

    add my recipes page

---
+++ Pages/MyRecipes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProjektDotnet.Data;
+using ProjektDotnet.Models;
+
+namespace ProjektDotnet.Pages
+{
+    public class MyRecipes : PageModel
+    {
+        private readonly ILogger<MyRecipes> _logger;
+        private readonly ApplicationDbContext _context;
+        public List<Recipe> Recipes { get; set; }
+
+        public MyRecipes(ILogger<MyRecipes> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+             Recipes = await _context.Recipe.Where(r=>r.User.UserName==User.Identity.Name).ToListAsync();
+        }
+    }
+}
