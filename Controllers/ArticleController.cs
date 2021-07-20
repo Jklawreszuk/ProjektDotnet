c5630a7f82a806865217a21c7e8781f59b0d3d85
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-07-20 13:54:32 +0200

    Create MVC pages

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,106 @@
+using System.Linq;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjektDotnet.Data;
+using ProjektDotnet.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProjektDotnet.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ILogger<ArticleController> _logger;
+        private readonly ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        public Recipe Recipe { get; set; }
+
+        public ArticleController(ILogger<ArticleController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet("Article/{id}", Name = "Article")]
+        public IActionResult Index(int id)
+        {
+            Recipe = _context.Recipe
+                .Include(r => r.User).Include(r => r.Images).Include(r => r.Ingredients).Where(n => n.Id == id).First();
+            return View(Recipe);
+        }
+
+        public IActionResult AddToFavourites([FromForm] int RowId)
+        {
+            var fav = new Favourites()
+            {
+                RecipeId = RowId,
+                User = _userManager.FindByNameAsync(User.Identity.Name).Result
+            };
+
+            var result = _context.Favourites.Find(_userManager.FindByNameAsync(User.Identity.Name).Result.Id, RowId);
+
+            if (result == null)
+            {
+                _context.Add(fav);
+                _context.SaveChanges();
+            }
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+        [HttpPost, Authorize]
+        public IActionResult Like([FromForm] int RowId)
+        {
+            var ratings = new Ratings()
+            {
+                RecipeId = RowId,
+                User = _userManager.FindByNameAsync(User.Identity.Name).Result,
+                IsLike = true
+            };
+
+            var result = _context.Ratings.Find( _userManager.FindByNameAsync(User.Identity.Name).Result.Id,RowId);
+
+            if (result == null)
+            {
+                _context.Add(ratings);
+                Recipe = _context.Recipe
+                .Include(r => r.User).Include(r => r.Images).Include(r => r.Ingredients).Where(n => n.Id == RowId).First();
+                Recipe.LikeCount += 1;
+                _context.Update(Recipe);
+                _context.SaveChanges();
+            }
+
+
+            return Redirect(Request.Headers["Referer"].ToString());
+
+        }
+        [HttpPost, Authorize]
+        public IActionResult Dislike([FromForm] int RowId)
+        {
+            var ratings = new Ratings()
+            {
+                RecipeId = RowId,
+                User = _userManager.FindByNameAsync(User.Identity.Name).Result,
+                IsLike = false
+            };
+
+            var result = _context.Ratings.Find(_userManager.FindByNameAsync(User.Identity.Name).Result.Id,RowId);
+
+            if (result == null)
+            {
+                Recipe = _context.Recipe
+                .Include(r => r.User).Include(r => r.Images).Include(r => r.Ingredients).Where(n => n.Id == RowId).First();
+                _context.Add(ratings);
+                Recipe.DislikeCount += 1;
+                _context.Update(Recipe);
+                _context.SaveChanges();
+            }
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+    }
+}
--- Models/Favourites.cs
+++ Models/Favourites.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ProjektDotnet.Models
+﻿namespace ProjektDotnet.Models
 {
     public class Favourites
     {
